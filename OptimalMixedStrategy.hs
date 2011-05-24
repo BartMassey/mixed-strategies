7ebab488ea3b52af71ba2153a1511cb7c4c55267
Bart Massey <bart@cs.pdx.edu>
2011-05-24 15:03:21 -0700

    added license

--- OptimalMixedStrategy.hs
+++ OptimalMixedStrategy.hs
@@ -1,4 +1,7 @@
 -- Copyright © 2011 Bart Massey
+-- [This program is licensed under the "MIT License"]
+-- Please see the file COPYING in the source
+-- distribution of this software for license terms.
 
 -- | Calculate an optimal mixed strategy given the payoff
 -- matrix of a two-player zero-sum single-round iterated
